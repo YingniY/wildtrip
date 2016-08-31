8aa1cf0a25a52f56252ef441fb8f16ace448c0d3
yying <jleongyy30@gmail.com>
2016-08-31 13:22:09 +0000

    styling, landing page design

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,7 @@
 class Idea < ActiveRecord::Base
   #scope :country, -> (country) { where("country like ?", "#{country}%")}
   scope :status, -> (status) { where country: country }
-  scope :activity, -> (activity) { where("LOWER(activity) like LOWER(?)", "%#{activity}%")}
+  scope :activity, -> (idea) { where("LOWER(activity) like LOWER(?)", "%#{activity}%")}
+  #scope :activity, -> (idea) { where activity: activity }
+
 end
