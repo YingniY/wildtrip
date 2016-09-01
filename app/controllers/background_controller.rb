ea94e2c395d187ed9f8bea3f045ddca8e87c9ef0
yying <jleongyy30@gmail.com>
2016-09-01 14:11:27 +0000

    random background controller, dropdown

---
+++ app/controllers/background_controller.rb
@@ -0,0 +1,4 @@
+class BackgroundController < ApplicationController
+  def image
+    redirect_to "/assets/images/background_#{rand(2)}.jpg"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'ideas/search' => 'ideas#search'
   resources :ideas
+  get '/random_background.jpg', to: 'background#image'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
